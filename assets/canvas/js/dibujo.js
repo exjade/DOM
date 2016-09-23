1a16428b2fc61110934b9329827e40448d61e2d5
Adrian Zamora <adrianzamora10@outlook.com>
2016-09-23 16:36:12 -0600

    drawing second line

--- assets/canvas/js/dibujo.js
+++ assets/canvas/js/dibujo.js
@@ -3,5 +3,15 @@ var lienzo = d.getContext("2d");
 console.log(lienzo);
 
 lienzo.beginPath();
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50, 10);
+lienzo.lineTo(250, 2);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
 lienzo.strokeStyle = "red";
-lienzo. moveTo(100,100);
+lienzo.moveTo(180, 10);
+lienzo.lineTo(290, 200);
+lienzo.stroke();
+lienzo.closePath();
