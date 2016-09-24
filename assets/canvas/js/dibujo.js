04d9669ffbac2005d585663c90419bfa00a2e4b9
Adrian Zamora <adrianzamora10@outlook.com>
2016-09-24 17:04:18 -0600

    for - JavaScript

--- assets/canvas/js/dibujo.js
+++ assets/canvas/js/dibujo.js
@@ -3,17 +3,18 @@ var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
 var yi, xf;
+var colorcito = "#FAA";
 
-while(l < lineas)
+for(l=0; l < lineas; l++)
 {
   yi = 10 * l;
   xf = 10 * (l + 1);
-  dibujarLinea("#AAF", 0 , yi, xf, 300);
+  dibujarLinea(colorcito, 0 , yi, xf, 300);
   console.log("linea " + l);
-  l = l + 1;
 }
-  dibujarLinea("#AFA", 1, 1, 1, 300);
-  dibujarLinea("#AFA", 1, 299, 299, 299);
+
+  dibujarLinea(colorcito, 1, 1, 1, 299);
+  dibujarLinea(colorcito, 1, 299, 299, 299);
 
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
